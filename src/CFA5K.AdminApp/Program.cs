fdb7b3f319604dbef891ac5f6c9c6374eac2434e
Eugene Bekker <ebekker@users.noreply.github.com>
2024-02-05 22:18:20 -0500

    adding admin app with some initial DB interactions

---
+++ src/CFA5K.AdminApp/Program.cs
@@ -0,0 +1,50 @@
+﻿// Chapman Farm Almost 5K.
+// Copyright (C) Eugene Bekker.
+
+namespace CFA5K.AdminApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var devLocalPath = Path.Combine(Directory.GetCurrentDirectory(),
+            "_IGNORE/appsettings.LOCAL.json");
+        if (File.Exists(devLocalPath))
+        {
+            Console.WriteLine("*** FOUND DEV LOCAL APPSETTINGS");
+            builder.Configuration.AddJsonFile(devLocalPath, optional: true);
+        }
+
+        builder.Services.AddDbContextFactory<AppDbContext>((services, optionsBuilder) =>
+        {
+            var constr = builder.Configuration.GetConnectionString(
+                AppDbContext.DefaultConnectionStringName);
+            optionsBuilder.UseNpgsql(constr);
+        });
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+            app.UseHttpsRedirection();
+        }
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
---
+++ src/CFA5K.AdminApp/_usings.cs
@@ -0,0 +1,9 @@
+﻿// Chapman Farm Almost 5K.
+// Copyright (C) Eugene Bekker.
+
+global using Microsoft.Extensions.Logging;
+global using Microsoft.EntityFrameworkCore;
+
+global using CFA5K.AdminApp.Components;
+global using CFA5K.AppDb;
+global using CFA5K.AppDb.Entities;
