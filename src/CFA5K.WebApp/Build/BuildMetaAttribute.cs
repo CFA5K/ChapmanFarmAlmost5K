17d17a306d6417b63676a67d1ad83f8df5dbf00c
Eugene Bekker <ebekker@users.noreply.github.com>
2024-02-10 14:16:47 -0500

    adding version info

---
+++ src/CFA5K.WebApp/Build/BuildMetaAttribute.cs
@@ -0,0 +1,20 @@
+﻿// Chapman Farm Almost 5K.
+// Copyright (C) Eugene Bekker.
+
+namespace CFA5K.WebApp.Build;
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
+public class BuildMetaAttribute : Attribute
+{
+    public DateTime? Date { get; set; }
+
+    public string? Hash { get; set; }
+
+    public string? Host { get; set; }
+
+    public string? User { get; set; }
+
+    public string? Path { get; set; }
+
+    public string? Conf { get; set; }
+}
